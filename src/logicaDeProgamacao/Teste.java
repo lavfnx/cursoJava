ee83fd23b09989531164261a260d211f949fde79
LAVINIA FERNANDES BARBOSA <lavinia.fbarbosa@senacsp.edu.br>
2024-01-23 12:02:53 -0300

    Criação da classe turma e objeto aluno, uso do construtor

--- src/logicaDeProgamacao/Teste.java
+++ src/logicaDeProgamacao/Teste.java
@@ -4,7 +4,7 @@ public class Teste {
 
 	public static void main(String[] args) {
 		int numero = 10;
-
+		
+		System.out.println();
 	}
-
 }
---
+++ src/pooTurma/Turma.java
@@ -0,0 +1,27 @@
+package pooTurma;
+
+public class Turma {
+	//Atributos da classe
+	String nome;
+	String telefone;
+	String email;
+	
+	//Construtor
+	public Turma() {
+		System.out.println("________________________________");
+	}
+
+	//Métodos
+		void ligarPc() {
+			System.out.println("Computador ligado");
+		}
+		
+		void logar() {
+			System.out.println("Login feito com sucesso");
+		}
+		
+		void desligarPc() {
+			System.out.println("Computador desligado");
+		}
+		
+		}
